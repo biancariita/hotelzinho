356aea403497161dbf1c15e01080950d5e3d9f8d
Bianca <biancariita@gmail.com>
2026-04-21 16:38:59 -0300

    corrigindo get_db

--- app/database.py
+++ app/database.py
@@ -1,20 +1,22 @@
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from dotenv import load_dotenv
 import os
-
-
-# Carrega variáveis do .env
-load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
-
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"sslmode": "require"}
 )
 
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
